2e1558533259126aca39247e5652ae9a9b3545e4
yumikotsunai <yumiko-tsunai@kke.co.jp>
2017-03-01 07:25:14 +0000

    connect callbackのDBのsaveのみを一旦コメントアウト

--- app/controllers/connecthookup_controller.rb
+++ app/controllers/connecthookup_controller.rb
@@ -66,21 +66,21 @@ class ConnecthookupController < ApplicationController
       }
       #begin
       
-      #if Connecttoken.find_by(key: key) == nil
-      #  connecttoken = Connecttoken.new(data)
-      #  connecttoken.save
-      #  puts "新しいものとして認識"
-      #else
-      #  connecttoken = Connecttoken.find_by(:key => key)
-      #  Connecttoken.update(connecttoken.id , :key => key,:access_token => j["access_token"] ,:refresh_token => j["refresh_token"] ,:expire => j["created_at"]+j["expires_in"],:updated_at => Time.now)
-      #  puts "更新する"
-      #end
-      #rescue
-      #  puts "データベースへの保存で問題が発生しました"
-      #end
+      if Connecttoken.find_by(key: key) == nil
+        connecttoken = Connecttoken.new(data)
+        #connecttoken.save
+        puts "新しいものとして認識"
+      else
+        connecttoken = Connecttoken.find_by(:key => key)
+        Connecttoken.update(connecttoken.id , :key => key,:access_token => j["access_token"] ,:refresh_token => j["refresh_token"] ,:expire => j["created_at"]+j["expires_in"],:updated_at => Time.now)
+        puts "更新する"
+      end
+      rescue
+        puts "データベースへの保存で問題が発生しました"
+      end
       
-      #@res = connecttoken
-      @res = "トークン"
+      @res = connecttoken
+      #@res = "トークン"
       @state = "認証に成功しました"
       render
       
