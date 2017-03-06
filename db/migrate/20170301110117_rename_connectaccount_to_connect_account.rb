19fe9558a8186f1af532125431fa5a713a00eef4
Mari Shibuya <mari@kke.co.jp>
2017-03-06 08:07:27 +0000

    モデル追加の再対応 DBをリセット後にcreate&migrateしてください

--- db/migrate/20170301110117_rename_connectaccount_to_connect_account.rb
+++
@@ -1,5 +0,0 @@
-class RenameConnectaccountToConnectAccount < ActiveRecord::Migration
-  def change
-    rename_table :connectaccounts, :connect_accounts
-  end
-end
--- db/migrate/20170301110203_rename_connecttoken_to_connect_token.rb
+++
@@ -1,5 +0,0 @@
-class RenameConnecttokenToConnectToken < ActiveRecord::Migration
-  def change
-    rename_table :connecttokens, :connect_tokens
-  end
-end
